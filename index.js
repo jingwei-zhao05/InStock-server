87289f3b5f27689da7e08f0454ace1774915c4eb
Jingwei Zhao <jingwei.zhao0527@gmail.com>
2023-06-05 22:02:12 -0400

    Create instock server folder structure and instill required dependencies

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const axios = require("axios");
+
+const { PORT, BACKEND_URL, CORS_ORIGIN } = process.env;
+
+app.use(cors({ origin: CORS_ORIGIN }));
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Welcome to /");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
