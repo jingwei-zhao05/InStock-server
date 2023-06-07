43c7b8fd6c8eb544d2323e77c85ddaed9380302d
Jingwei Zhao <jingwei.zhao0527@gmail.com>
2023-06-06 22:07:57 -0400

    Add findWarehouse and editWarehouse two functions in warehouse controller

--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const warehouseController = require("../controllers/warehouse-controller");
 
-router.route("/").get(warehouseController.getWarehouse);
+router.route("/").get(warehouseController.getWarehouses);
+router
+  .route("/:id")
+  .get(warehouseController.findWarehouse)
+  .put(warehouseController.editWarehouse);
 
 module.exports = router;
