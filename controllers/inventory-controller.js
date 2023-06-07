77696c8122c50f98816e7225cad9a46487ed193a
Aia_makhruka <aia.makhruka@gmail.com>
2023-06-07 16:03:50 -0400

    adds the inventorylist route and joins the warehouse table with the inventory in controller

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -1,13 +1,35 @@
 const knex = require("knex")(require("../knexfile"));
 
-const getInventory = (_req, res) => {
-  knex("inventories")
-    .then((data) => {
-      res.status(200).json(data);
+// const getInventory = (_req, res) => {
+//   knex("inventories")
+//     .then((data) => {
+//       res.status(200).json(data);
+//     })
+//     .catch((err) => res.status(400).send(`Error retrieving Users: ${err}`));
+// };
+
+const getJoinedTable = (req, res) => {
+  knex
+    .from("inventories")
+    .select(
+      "inventories.item_name",
+      "inventories.category",
+      "inventories.status",
+      "inventories.quantity",
+      "warehouse_name"
+    )
+    .join("warehouses", "inventories.warehouse_id", "warehouses.id")
+    .then((joined) => {
+      res.status(200).json(joined);
     })
-    .catch((err) => res.status(400).send(`Error retrieving Users: ${err}`));
+    .catch((error) => {
+      res.status(500).json({
+        message: `Unable to retrieve inventory data. Error: ${error}`,
+      });
+    });
 };
 
 module.exports = {
-  getInventory,
+  // getInventory,
+  getJoinedTable,
 };
--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -8,6 +8,27 @@ const getWarehouse = (_req, res) => {
     .catch((err) => res.status(400).send(`Error retrieving Users: ${err}`));
 };
 
+const getWarehouseDetail = (req, res) => {
+  knex("warehouses")
+    .where({ id: req.params.id })
+    .then((findWarehouse) => {
+      if (findWarehouse.length === 0) {
+        // Return 404 if the warehouse is not found
+        return res
+          .status(404)
+          .json({ error: `Warehouse with ID: ${req.params.id} not found` });
+      }
+
+      res.json(findWarehouse[0]);
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: "Unable to retrieve warehouse data",
+      });
+    });
+};
+
 module.exports = {
   getWarehouse,
+  getWarehouseDetail,
 };
