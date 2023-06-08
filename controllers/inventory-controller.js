b82f49cc32c9abd2a868497a9defe0d40460850b
Thomas Vovk <127060122+Thomasvovk@users.noreply.github.com>
2023-06-08 11:54:06 -0400

    completedback-end-inventory-controller-and-routes

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -8,6 +8,25 @@ const getInventories = (_req, res) => {
     .catch((err) => res.status(400).send(`Error retrieving Users: ${err}`));
 };
 
+const findInventoryItem = (req, res) => {
+  knex("inventories")
+    .where({ id: req.params.id })
+    .then((inventoryItemFound) => {
+      if (inventoryItemFound.length === 0) {
+        return res.status(404).json({
+          message: `Inventory with ID ${req.params.id} not found`,
+        });
+      }
+      res.json(inventoryItemFound[0]);
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: `Unable to retrive inventory data with ID: ${req.params.id}`,
+      });
+    });
+};
+
 module.exports = {
   getInventories,
+  findInventoryItem,
 };
--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const inventoryController = require("../controllers/inventory-controller");
 
 router.route("/").get(inventoryController.getInventories);
+router.route("/:id").get(inventoryController.findInventoryItem);
 
 module.exports = router;
